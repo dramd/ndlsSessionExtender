426908a5823890fb7115d45e5cd718f1c52be5be
Ash Dorey <ash.dorey@me.com>
2024-05-31 15:31:21 +0100

    Add some debug logging & reporting of errors. Create prototype database on startup. Auto fill user id & preserve roles where possible. Added a build option to update the existing database in place without replacing it with the prototype. Support macOS 10.12 deployment target.

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -2,10 +2,37 @@
 #include "CouchbaseLite.h"
 #include "globaldb.h"
 
-static juce::File getEndlesssGlobalDatabase() 
+
+static juce::File getGlobalDatabaseContainingFolder()
 {
-    return juce::File::getSpecialLocation(juce::File::SpecialLocationType::userApplicationDataDirectory).getChildFile("Endlesss").getChildFile("production").getChildFile("Data").getChildFile("global.cblite2");
+    auto file = juce::File::getSpecialLocation(juce::File::SpecialLocationType::userApplicationDataDirectory);
+#ifdef JUCE_MAC
+    if(file.getChildFile("Application Support").exists())
+    {
+        file = file.getChildFile("Application Support");
+    }
+#endif
+    if(file.getChildFile("Endlesss").exists())
+    {
+        file = file.getChildFile("Endlesss");
+    }
+    if(file.getChildFile("production").exists())
+    {
+        file = file.getChildFile("production");
+    }
+    if(file.getChildFile("Data").exists())
+    {
+        file = file.getChildFile("Data");
+    }
+
+    return file;
+}
+
+static juce::File getEndlesssGlobalDatabase()
+{
+    return getGlobalDatabaseContainingFolder().getChildFile("global.cblite2");
 };
+
 
 static juce::File getGlobalDatabasePrototype()
 {
@@ -18,15 +45,19 @@ static juce::File getGlobalDatabasePrototype()
 
 static juce::File writeGlobalDbFile ()
 {
-    if(getGlobalDatabasePrototype().existsAsFile())
+    DBG("writeGlobalDbFile");
+    if(getGlobalDatabasePrototype().existsAsFile()){
+        DBG("Global Database Prototype already exists @ " << getGlobalDatabasePrototype().getFullPathName());
         return getGlobalDatabasePrototype();
-    
+    }
     auto destinationFile = getGlobalDatabasePrototype();
+    DBG("Creating Global Database File from binary data @ " << destinationFile.getFullPathName());
     {
         auto outputStream = destinationFile.createOutputStream();
         outputStream->write(db_sqlite3, db_sqlite3Size);
         outputStream->flush();
     }
+    DBG("Created Global Database File from binary data @ " << destinationFile.getFullPathName());
     return destinationFile;
 }
 
@@ -48,8 +79,27 @@ setExpires
     juce::DynamicObject &doc_session,
     juce::Time           time
 ) {
+    DBG("Setting expiry date");
     doc_session.setProperty("expires", getY2038().toMilliseconds());
     return juce::Result::ok();
+}
+
+static
+juce::Result
+setUser
+(
+    juce::DynamicObject &doc_session,
+    juce::String           userId
+) {
+    if(userId.isNotEmpty() && userId.containsNonWhitespaceChars()){
+        userId = userId.toLowerCase().trim();
+        if(doc_session.getProperty("user_id") != userId){
+            DBG("Setting user id: " << userId);
+            doc_session.setProperty("user_id", userId);
+        }
+        return juce::Result::ok();
+    }
+    return juce::Result::fail("Invalid User ID");
 }
 
 static
@@ -88,7 +138,8 @@ createSession
     obj->setProperty("type",           "Session");
     obj->setProperty("app_version",    10000);
 
-    obj->setProperty("user_id",        user_id);
+    setUser(*obj, user_id);
+
     obj->setProperty("roles",          roles);
     obj->setProperty("profile",        juce::var(profile_obj));
 
@@ -113,9 +164,11 @@ juce::Result
 createActiveSession
 (
     db::CouchbaseLiteDatabase &db,
-    juce::String               user_id
+    juce::String               user_id,
+    juce::StringArray          roles
 ) {
-    juce::var session_data = createSession(user_id);
+    DBG("Creating Active Session for " << user_id);
+    juce::var session_data = createSession(user_id, {}, roles);
 
     juce::DynamicObject *doc_obj = session_data.getDynamicObject();
     doc_obj->setProperty("_id", "ActiveSession");
@@ -138,31 +191,160 @@ copyPrototypeDbWithBackup
     juce::File source,
     juce::File destination
 ) { 
-    writeGlobalDbFile ();
     juce::File destination_backup = destination.withFileExtension(".sqlite3.backup");
 
     if (!destination_backup.existsAsFile())
     {
+        DBG("Doing first run copy of original database file");
         // First time use! This means the current version of the global DB is precious.
         // Let's have it as an extra backup on the side.
         juce::File vip_backup = destination.withFileExtension(".sqlite3.original");
         destination.copyFileTo(vip_backup);
     }
+    DBG("Making backup of database file");
     destination.copyFileTo(destination_backup);
-
-    source.copyFileTo(destination);
+    if(source != destination){
+        DBG("Copying " << source.getFullPathName() << " to " << destination.getFullPathName());
+        source.copyFileTo(destination);
+    }
+}
+juce::Result getActiveUser(db::CouchbaseLiteDatabase& db, juce::String& username)
+{
+    try
+    {
+        DBG("Attempting to get currently active user id");
+        juce::var document = db.getLocalDocument("ActiveSession");
+        if(!document.isObject())
+        {
+            return juce::Result::fail("Did not find an active session");
+        }
+        if(document.hasProperty("user_id"))
+        {
+            username = document["user_id"];
+        }
+        else if(document.hasProperty("user"))
+        {
+            username = document["user"];
+        }
+        
+        return juce::Result::ok();
+    }
+    catch (std::exception& e)
+    {
+        return juce::Result::fail("Exception occurred: " + juce::String(e.what()));
+    }
+    catch (...)
+    {
+        return juce::Result::fail("Unknown exception occured");
+    }
+}
+juce::Result getRoles(db::CouchbaseLiteDatabase& db, juce::StringArray& roles)
+{
+    try
+    {
+        DBG("Attempting to get currently active user id");
+        juce::var document = db.getLocalDocument("ActiveSession");
+        if(!document.isObject())
+        {
+            roles = defaultRoles();
+            return juce::Result::fail("Did not find an active session");
+        }
+        if(document.hasProperty("roles"))
+        {
+            if(document["roles"].isString())
+                roles.add(document["roles"]);
+            else if(document["roles"].isArray())
+            {
+                auto rolesVar = *document["roles"].getArray();
+                for(auto role : rolesVar)
+                {
+                    if(role.isString())
+                        roles.addIfNotAlreadyThere(role);
+                }
+            }
+        }
+        else
+        {
+            roles = defaultRoles();
+        }
+        return juce::Result::ok();
+    }
+    catch (std::exception& e)
+    {
+        return juce::Result::fail("Exception occurred: " + juce::String(e.what()));
+    }
+    catch (...)
+    {
+        return juce::Result::fail("Unknown exception occured");
+    }
 }
 
+juce::Result updateActiveSession(db::CouchbaseLiteDatabase& db, const juce::String& username)
+{
+    try {
+        DBG("Updating active session for " << username);
+
+        juce::var document = db.getLocalDocument("ActiveSession");
+        if(!document.isObject())
+        {
+            return juce::Result::fail("Did not find an active session");
+        }
+        
+        if(juce::DynamicObject *object = document.getDynamicObject())
+        {
+            setUser(*object, username);
+            setExpires(*object, getY2038());
+        }
+        int num_rows_modified = db.setLocalDocument(document);
+        
+        if (num_rows_modified > 0)
+        {
+            return juce::Result::ok();
+        }
+
+        return juce::Result::fail("Session document could not be updated");
+    }
+    catch (std::exception& e)
+    {
+        return juce::Result::fail("Exception occurred: " + juce::String(e.what()));
+    }
+    catch (...)
+    {
+        return juce::Result::fail("Unknown exception occured");
+    }
+}
 
 //==============================================================================
 MainComponent::MainComponent()
 {
     setSize (320, 240);
     auto& lnf = getLookAndFeel();
-
+    DBG("Initialising");
+    roles = defaultRoles();
     if(getEndlesssGlobalDatabase().exists())
     {
-        db = std::make_unique<db::CouchbaseLiteDatabase>(getEndlesssGlobalDatabase());
+        try 
+        {
+            DBG("Found global database on startup");
+            db = std::make_unique<db::CouchbaseLiteDatabase>(getEndlesssGlobalDatabase());
+            juce::String username;
+            getActiveUser(*db, username);
+            getRoles(*db, roles);
+            if(username.isNotEmpty())
+            {
+                DBG("Found existing session with user id "<< username);
+                editor_username.setText(username, juce::dontSendNotification);
+            }
+            writeGlobalDbFile ();
+        }
+        catch(std::exception e)
+        {
+            DBG("Startup Exception: " + juce::String(e.what()));
+        }
+        catch(...)
+        {
+            DBG("Startup Exception: Unknown");
+        }
         syncUiState();
     }
     
@@ -177,20 +359,35 @@ MainComponent::MainComponent()
                     return;
                 }
 
-                if(!db)
-                {
-                    return;
-                }
-
-                copyPrototypeDbWithBackup(getGlobalDatabasePrototype(), getEndlesssGlobalDatabase().getChildFile("db.sqlite3"));
-
-                juce::Result updateResult = createActiveSession(*db, editor_username.getText());
-
+                juce::Result updateResult { juce::Result::fail("Failed to open database") };
                 
+                if(db)
+                {
+                    try
+                    {
+#if UPDATE_WITHOUT_REPLACING
+                        copyPrototypeDbWithBackup(getEndlesssGlobalDatabase().getChildFile("db.sqlite3"), getEndlesssGlobalDatabase().getChildFile("db.sqlite3"));
+                        updateResult = updateActiveSession(*db, editor_username.getText());
+#else
+                        copyPrototypeDbWithBackup(getGlobalDatabasePrototype(), getEndlesssGlobalDatabase().getChildFile("db.sqlite3"));
+                        
+                        updateResult = createActiveSession(*db, editor_username.getText(), roles);
+#endif
+                    } 
+                    catch(std::exception e)
+                    {
+                        updateResult = juce::Result::fail(e.what());
+                    } 
+                    catch(...)
+                    {
+                        updateResult = juce::Result::fail("Unknown Exception");
+                    }
+                }
                 juce::MessageBoxOptions opts;
 
                 if(updateResult.failed())
                 {
+                    DBG("Error: " << updateResult.getErrorMessage());
                     opts = opts.withIconType(juce::MessageBoxIconType::WarningIcon)
                     .withTitle("Error")
                     .withMessage(updateResult.getErrorMessage())
@@ -228,7 +425,7 @@ MainComponent::MainComponent()
             fileChooser = std::make_unique<juce::FileChooser> 
             (
                 "Please select Endlesss global.cblite2",
-                juce::File::getSpecialLocation (juce::File::userApplicationDataDirectory),
+                getGlobalDatabaseContainingFolder(),
                 "*.cblite2",
                 true,
                 true
@@ -241,8 +438,28 @@ MainComponent::MainComponent()
                 juce::File databaseFile (chooser.getResult());
                 if(databaseFile.exists())
                 {
-                    db = std::make_unique<db::CouchbaseLiteDatabase>(databaseFile);
-                    syncUiState();
+                    try {
+                        DBG("Found global database provided by user");
+                        db = std::make_unique<db::CouchbaseLiteDatabase>(databaseFile);
+                        juce::String username;
+                        getActiveUser(*db, username);
+                        getRoles(*db, roles);
+                        if(username.isNotEmpty())
+                        {
+                            DBG("Found existing session with user id "<< username);
+                            editor_username.setText(username, juce::dontSendNotification);
+                        }
+                        syncUiState();
+                    }
+                    catch(std::exception e)
+                    {
+                        DBG(e.what());
+                    } 
+                    catch(...)
+                    {
+                        DBG("Unknown Exception");
+                    }
+
                 }
             });
         }
@@ -279,8 +496,10 @@ MainComponent::~MainComponent()
 
 void MainComponent::syncUiState()
 {
+    DBG("syncUiState");
     if(db)
     {
+        DBG("Showing Create Session button");
         editor_username.setEnabled(true);
         editor_username.setTextToShowWhenEmpty("Username", juce::Colours::grey);
 
@@ -295,32 +514,13 @@ void MainComponent::syncUiState()
     }
     else
     {
+        DBG("Showing browse for global.cblite2");
         editor_username.setEnabled(false);
         editor_username.setTextToShowWhenEmpty("Use the button below to browse for your local database file", juce::Colours::grey);
         btn_apply.setButtonText("Browse for global.cblite2");
     }
 }
 
-juce::Result MainComponent::updateActiveSession()
-{
-    if(!db)
-    {
-        return juce::Result::fail("Database file could not be opened");
-    }
-
-    juce::var document = db->getLocalDocument("ActiveSession");
-    if(!document.isObject())
-    {
-        return juce::Result::fail("Did not find an active session");
-    }
-
-    if(juce::DynamicObject *object = document.getDynamicObject())
-    {
-        setExpires(*object, getY2038());
-    }
-    db->setLocalDocument(document);
-    return juce::Result::ok();
-}
 //==============================================================================
 void MainComponent::paint (juce::Graphics& g)
 {
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -19,11 +19,11 @@ public:
     
     void syncUiState();
 
-    juce::Result updateActiveSession();
 
 private:
     juce::TextEditor editor_username; 
     juce::TextButton btn_apply { "Go!" };
+    juce::StringArray roles;
     std::unique_ptr<db::CouchbaseLiteDatabase> db;
     std::unique_ptr<juce::FileChooser> fileChooser;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
